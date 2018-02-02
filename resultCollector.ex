1f4bfe6f10fdc153ede79cd549e423b8addd8aad
Valery Baturin <valeryxbaturin@gmail.com>
2018-02-02 19:44:04 +0300

    adds result collector as a module

---
+++ resultCollector.ex
@@ -0,0 +1,18 @@
+defmodule ResultCollector do
+    def collect(n, dad_pid) do
+        spawn(fn -> receiver(n, [], dad_pid)end)
+    end
+
+    def receiver(collection_size, acc, dad_pid)
+        when collection_size == length(acc) do
+            send dad_pid, {:finish, acc}
+        end
+
+    def receiver(collection_size, acc, dad_pid)
+        when collection_size > length(acc) do
+        receive do
+            {:ok, result} ->
+                receiver(collection_size, acc ++ result, dad_pid)
+        end
+    end
+end
